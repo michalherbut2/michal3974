bb181552048792f08db4f6f7ea35b5814100421a
Michal <michal@michalarch.local>
2023-11-02 21:16:15 +0100

    add nb command nad update .gitignore file

--- computings/checkInactivity.js
+++ computings/checkInactivity.js
@@ -21,11 +21,13 @@ module.exports = client => {
 
   // const intervalInMilliseconds = 10_000; // 10 sec
   const intervalInMilliseconds = 86_400_000; // 1 day
-  const weekInMilliseconds = 7 * intervalInMilliseconds; // Tydzień w milisekundach
+  // const weekInMilliseconds = 7 * intervalInMilliseconds; // Tydzień w milisekundach
 
-  setInterval(() => {
+  setInterval(async () => {
+    const commandsChannel = await client.channels.cache.get("1124032895263195199");
+    commandsChannel.send("Dodano nieobecności adminom!");
     guild.members.fetch().then(members => {
-      members.forEach(member => {
+      members.forEach(async member => {
         const hasRole = member.roles.cache.some(role => role.id === ROLE_ID);
         if (hasRole) {
           incrementUserInactivity(member.user.id);
@@ -36,11 +38,18 @@ module.exports = client => {
             [member.user.id],
             (err, row) => {
               if (err)
-                console.error("Błąd podczas pobierania informacji o użytkowniku:",err);
+                console.error(
+                  "Błąd podczas pobierania informacji o użytkowniku:",
+                  err
+                );
               if (row && row.inactivity_days >= 7) {
                 member.roles
                   .remove(role)
-                  .then(() => console.log(`Usunięto rolę ${role.name} od użytkownika ${member.user.tag}.`))
+                  .then(() =>
+                    console.log(
+                      `Usunięto rolę ${role.name} od użytkownika ${member.user.tag}.`
+                    )
+                  )
                   .catch(console.error);
                 resetUserInactivity(member.user.id);
               }
--- events/ready.js
+++ events/ready.js
@@ -4,16 +4,17 @@ const updateList = require("../computings/updateList");
 module.exports = {
     name: 'ready',
     once: true,
-    execute(bot) {
-      //Log Bot's username and the amount of servers its in to console
+    execute(client) {
+      //Log client's username and the amount of servers its in to console
       console.log(
-        `${bot.user.username} is online on ${bot.guilds.cache.size} servers!`
+        `${client.user.username} is online on ${client.guilds.cache.size} servers!`
       );
 
-      //Set the Presence of the bot user
-      bot.user.setPresence({ activities: [{ name: "Jak tam pogoda?" }] });
-      updateList(bot)
-      checkInactivity(bot);
+      //Set the Presence of the client user
+      client.user.setPresence({ activities: [{ name: "Jak tam pogoda?" }] });
+      updateList(client)
+      checkInactivity(client);
       // bot.user.setAvatar("img/logo.jpg");
+      // client.commands.get("nb").run(client, "","");
     }
 }
