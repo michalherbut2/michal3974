0cf28f1105378d96214ec311c4c36528b46fbeca
Michal <michal@michalarch.local>
2023-11-13 22:19:56 +0100

    change printing nb

--- slashCommands/xd/nb.js
+++ slashCommands/xd/nb.js
@@ -11,15 +11,15 @@ module.exports = {
     const rows = db.prepare("SELECT * FROM users").all();
 
     let index = 1;
-    let mes = "Lista nieobecnośći (7 nieobecności utrata admina):";
+    let mes = "```Lista nieobecnośći (7 nieobecności utrata admina):";
     for (const row of rows) {
-      mes += `\n${index++}. ${await getNick(interaction, row.user_id)} - ${
+      mes += `\n${index++}. ${(await getNick(interaction, row.user_id)).padEnd(19)} - ${
         row.inactivity_days
       } nieobecności`;
     }
 
     db.close();
-    await interaction.reply(mes);
+    await interaction.reply(mes+"```");
   },
 };
 
