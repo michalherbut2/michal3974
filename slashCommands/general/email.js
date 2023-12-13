02b42554c53c348c62bcdda7f5bb00bc199227e5
PatrykPatryk5 <128894397+PatrykPatryk5@users.noreply.github.com>
2023-12-13 15:56:06 +0100

    Update email.js

--- slashCommands/general/email.js
+++ slashCommands/general/email.js
@@ -3,7 +3,16 @@ const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const nodemailer = require('nodemailer');
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'twoj-email@gmail.com', // Zastąp swoim adresem e-mail
+    pass: 'twoje-haslo', // Zastąp swoim hasłem
+  },
+});
+
+const clientId = 'TWÓJ_ID_BOTA'; // Zastąp swoim ID bota
+const guildId = 'TWÓJE_ID_GUILDII'; // Zastąp swoim ID serwera/gildii
 
 const commands = [
   {
@@ -12,8 +21,8 @@ const commands = [
     options: [
       {
         name: 'recipient',
-        type: 'USER',
-        description: 'Użytkownik, do którego ma być wysłany e-mail',
+        type: 'STRING',
+        description: 'Adres e-mail odbiorcy',
         required: true,
       },
       {
@@ -32,9 +41,6 @@ const commands = [
   },
 ];
 
-const clientId = 'TWÓJ_ID_BOTA'; // Zastąp swoim ID bota
-const guildId = 'TWÓJE_ID_GUILDII'; // Zastąp swoim ID serwera/gildii
-
 const rest = new REST({ version: '9' }).setToken('TWÓJ_TOKEN_BOTA');
 
 (async () => {
@@ -52,40 +58,35 @@ const rest = new REST({ version: '9' }).setToken('TWÓJ_TOKEN_BOTA');
   }
 })();
 
-client.on('interactionCreate', async (interaction) => {
-  if (!interaction.isCommand()) return;
+const handleSendEmail = async (interaction) => {
+  const recipientEmail = interaction.options.getString('recipient');
+  const subject = interaction.options.getString('subject');
+  const content = interaction.options.getString('content');
 
-  const { commandName, options } = interaction;
+  const mailOptions = {
+    from: 'twoj-email@gmail.com', // Zastąp swoim adresem e-mail
+    to: recipientEmail,
+    subject: subject,
+    text: content,
+  };
 
-  if (commandName === 'sendemail') {
-    const recipient = options.getUser('recipient');
-    const subject = options.getString('subject');
-    const content = options.getString('content');
-
-    const transporter = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-        user: 'twoj-email@gmail.com', // Zastąp swoim adresem e-mail
-        pass: 'twoje-haslo', // Zastąp swoim hasłem
-      },
-    });
-
-    const mailOptions = {
-      from: 'twoj-email@gmail.com', // Zastąp swoim adresem e-mail
-      to: recipient.email,
-      subject: subject,
-      text: content,
-    };
-
-    transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        console.error(error);
-        return interaction.reply('Wystąpił błąd podczas wysyłania e-maila.');
-      }
-      console.log('E-mail wysłany: ' + info.response);
-      interaction.reply('E-mail został wysłany pomyślnie!');
-    });
+  try {
+    const info = await transporter.sendMail(mailOptions);
+    console.log('E-mail wysłany: ' + info.response);
+    await interaction.reply('E-mail został wysłany pomyślnie!');
+  } catch (error) {
+    console.error(error);
+    await interaction.reply('Wystąpił błąd podczas wysyłania e-maila.');
   }
-});
+};
 
-
+module.exports = {
+  data: {
+    name: 'sendemail',
+    description: 'Wysyła e-mail do określonego odbiorcy',
+    options: commands[0].options,
+  },
+  async execute(interaction) {
+    await handleSendEmail(interaction);
+  },
+};
