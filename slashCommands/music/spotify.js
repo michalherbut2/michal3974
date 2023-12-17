b299badf74522d64205a40fa57f8d92e22f512be
PatrykPatryk5 <128894397+PatrykPatryk5@users.noreply.github.com>
2023-12-17 18:12:37 +0100

    Update spotify.js
    poprawione

--- slashCommands/music/spotify.js
+++ slashCommands/music/spotify.js
@@ -2,64 +2,39 @@ const { joinVoiceChannel, createAudioPlayer, createAudioResource, AudioPlayerSta
 const spdl = require('spdl-core');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
+const { SlashCommandBuilder } = require('@discordjs/builders');
 
-const clientId = 'YOUR_CLIENT_ID';
-const guildId = 'YOUR_GUILD_ID';
-const token = 'YOUR_BOT_TOKEN';
+// Zastąp poniższe wartości swoimi prawdziwymi danymi lub użyj zmiennych środowiskowych
+const clientId = process.env.CLIENT_ID;
+const guildId = process.env.GUILD_ID;
+const token = process.env.BOT_TOKEN;
 
-const commands = [
-  {
-    name: 'spotify',
-    description: 'Play music from Spotify',
-    options: [
-      {
-        name: 'spotifyinput',
-        type: 'STRING',
-        description: 'Spotify link or search query',
-        required: true,
-      },
-    ],
-  },
-  {
-    name: 'playspotify',
-    description: 'Play music from Spotify',
-    options: [
-      {
-        name: 'link',
-        type: 'STRING',
-        description: 'Spotify link or search query',
-        required: true,
-      },
-    ],
-  },
-];
-
-const rest = new REST({ version: '10' }).setToken(token);
-
-const player = createAudioPlayer();
-let audioResource;
-
-async function playMusic(interaction, guildId) {
-  if (!interaction.isCommand()) return;
-
-  const { commandName, options } = interaction;
-
-  if (commandName === 'spotify' || commandName === 'playspotify') {
-    const spotifyInput = options.getString('spotifyinput') || options.getString('link');
+// Komenda Spotify
+const spotifyCommand = {
+  data: new SlashCommandBuilder()
+    .setName('spotify')
+    .setDescription('Odtwarzaj muzykę ze Spotify')
+    .addStringOption(option =>
+      option
+        .setName('spotifyinput')
+        .setDescription('Link lub zapytanie wyszukiwania na Spotify')
+        .setRequired(true)),
+  execute: async (interaction, guildId, options) => {
+    const spotifyInput = options.getString('spotifyinput');
 
     try {
       const isPlaylist = spdl.isPlaylist(spotifyInput);
       const trackList = isPlaylist ? await spdl.getTracks(spotifyInput) : [await spdl.getInfo(spotifyInput)];
 
       if (!trackList.length) {
-        throw new Error('No tracks found.');
+        throw new Error('Nie znaleziono utworów.');
       }
 
       const voiceChannel = interaction.member.voice.channel;
 
       if (!voiceChannel) {
         return interaction.reply({
-          content: 'Join a voice channel!',
+          content: 'Dołącz do kanału głosowego!',
           ephemeral: true,
         });
       }
@@ -71,12 +46,12 @@ async function playMusic(interaction, guildId) {
       });
 
       for (const trackInfo of trackList) {
-        audioResource = createAudioResource(trackInfo.audio, { inlineVolume: true });
+        const audioResource = createAudioResource(trackInfo.audio, { inlineVolume: true });
 
         voiceConnection.subscribe(player);
         player.play(audioResource);
 
-        // Event listener to detect when the playback ends
+        // Słuchacz zdarzeń do wykrywania zakończenia odtwarzania
         await new Promise((resolve) => {
           player.once(AudioPlayerStatus.Idle, () => {
             resolve();
@@ -87,30 +62,30 @@ async function playMusic(interaction, guildId) {
       voiceConnection.destroy();
 
       interaction.reply({
-        content: `Now playing: ${isPlaylist ? 'Playlist' : 'Track'} - **${trackList[0].title}** by **${trackList[0].artists.join(', ')}**`,
+        content: `Teraz odtwarzane: ${isPlaylist ? 'Playlista' : 'Utwór'} - **${trackList[0].title}** wykonawcy **${trackList[0].artists.join(', ')}**`,
       });
     } catch (error) {
-      console.error('Error:', error);
+      console.error('Błąd:', error);
       interaction.reply({
-        content: `An error occurred: ${error.message}`,
+        content: `Wystąpił błąd: ${error.message}`,
       });
     }
-  }
-}
+  },
+};
+
+const rest = new REST({ version: '10' }).setToken(token);
 
 (async () => {
   try {
-    console.log('Started refreshing application (/) commands.');
+    console.log('Rozpoczęto odświeżanie komend aplikacji (/).');
 
     await rest.put(
       Routes.applicationGuildCommands(clientId, guildId),
-      { body: commands },
+      { body: [spotifyCommand.data.toJSON()] },
     );
 
-    console.log('Successfully reloaded application (/) commands.');
+    console.log('Pomyślnie odświeżono komendy aplikacji (/).');
   } catch (error) {
     console.error(error);
   }
 })();
-
-// Możesz dodać kod inicjalizacji, jeśli to konieczne, np. w przypadku używania innych funkcji w tym pliku
