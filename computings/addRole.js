74f2c22eb8ca85e8671e378117b0dcd91ef1e806
PatrykPatryk5 <128894397+PatrykPatryk5@users.noreply.github.com>
2023-12-30 14:06:13 +0100

    Update addRole.js
    embed

--- computings/addRole.js
+++ computings/addRole.js
@@ -1,6 +1,8 @@
+const { MessageEmbed } = require('discord.js');
+
 module.exports = async (interaction, userId, roleId) => {
-  const guild = interaction.guild
-  const channel = interaction.channel
+  const guild = interaction.guild;
+  const channel = interaction.channel;
   const member = guild.members.cache.get(userId);
   const roleToAssign = guild.roles.cache.get(roleId);
 
@@ -8,18 +10,39 @@ module.exports = async (interaction, userId, roleId) => {
     console.error("Rola nie została znaleziona.");
     return;
   }
+
   if (member) {
     if (member.roles.cache.has(roleId)) {
-      channel.send("Masz już tę rolę.");
+      // Stworzenie osadzenia dla wiadomości "Masz już tę rolę"
+      const embed = new MessageEmbed()
+        .setColor('#FF0000') // Możesz dostosować kolor
+        .setTitle('Błąd')
+        .setDescription('Masz już tę rolę.');
+
+      channel.send({ embeds: [embed] });
     } else {
+      // Dodanie roli do członka
       member.roles
         .add(roleToAssign)
         .then(() => {
-          channel.send(`Dodano rolę ${roleToAssign?.name}!`);
+          // Stworzenie osadzenia dla wiadomości "Dodano rolę"
+          const embed = new MessageEmbed()
+            .setColor('#00FF00') // Możesz dostosować kolor
+            .setTitle('Sukces')
+            .setDescription(`Dodano rolę ${roleToAssign?.name}!`);
+
+          channel.send({ embeds: [embed] });
         })
-        .catch(error => {
-          channel.send("Wystąpił problem podczas próby przydzielenia roli.");
-          console.error("Role assignment error:", error);
+        .catch((error) => {
+          // Stworzenie osadzenia dla wiadomości błędu
+          const embed = new MessageEmbed()
+            .setColor('#FF0000') // Możesz dostosować kolor
+            .setTitle('Błąd')
+            .setDescription('Wystąpił problem podczas próby przydzielenia roli.');
+
+          channel.send({ embeds: [embed] });
+
+          console.error("Błąd podczas przydzielania roli:", error);
         });
     }
   }
