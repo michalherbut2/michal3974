3d368a3663908e22ac2d1aa3b2b016f1baf722c3
PatrykPatryk5 <128894397+PatrykPatryk5@users.noreply.github.com>
2024-01-12 18:19:47 +0100

    Update invite.js
    edycja na v14

--- commands/invite.js
+++ commands/invite.js
@@ -1,13 +1,15 @@
 const { MessageActionRow, MessageButton } = require("discord.js");
 
 module.exports = {
-  name: "invite",
-  description: "Invite the bot to your server",
-  category: "utility",
-  execute(bot, interaction) {
+  data: {
+    name: "invite",
+    description: "Invite the bot to your server",
+  },
+  async execute(interaction) {
+    const bot = interaction.client;
     const embed = bot.say.baseEmbed(interaction)
       .setAuthor({name: `${bot.user.tag}`})
-      .setDescription(`[Kliknij aby dodać.](https://discord.com/api/oauth2/authorize?client_id=${bot.user.id}&permissions=8&scope=applications.commands%20bot)`)
+      .setDescription(`Kliknij aby dodać.`)
       .setTimestamp();
     const row = new MessageActionRow().addComponents([
       new MessageButton()
@@ -16,7 +18,6 @@ module.exports = {
       .setURL(`https://discord.com/api/oauth2/authorize?client_id=${bot.user.id}&permissions=8&scope=applications.commands%20bot`)
     ]);
 
-
-    return interaction.reply({ ephemeral: true, embeds: [embed], components: [row] });
+    await interaction.reply({ ephemeral: true, embeds: [embed], components: [row] });
   }
 };
