f7d3c9daeab039809f07756748cd98311665c7a6
PatrykPatryk5 <128894397+PatrykPatryk5@users.noreply.github.com>
2024-01-12 18:21:28 +0100

    Update volume.js
    v14

--- commands/music/volume.js
+++ commands/music/volume.js
@@ -1,28 +1,30 @@
 module.exports = {
-  name: "volume",
-  description: "Ustaw głośność",
-  category: "filters",
-  usage: "<poziom>",
-  options: [
-    {
-      name: "poziom",
-      description: "Wybierz poziom głośności",
-      type: "INTEGER",
-      required: true,
-      choices: [
-        { name: "0%", value: 0 },
-        { name: "25%", value: 25 },
-        { name: "50%", value: 50 },
-        { name: "75%", value: 75 },
-        { name: "100%", value: 100 },
-        { name: "125%", value: 125 },
-        { name: "150%", value: 150 },
-        { name: "175%", value: 175 },
-        { name: "200%", value: 200 }
-      ],
-    },
-  ],
-  async execute(bot, interaction) {
+  data: {
+    name: "volume",
+    description: "Ustaw głośność",
+    type: "CHAT_INPUT",
+    options: [
+      {
+        name: "poziom",
+        description: "Wybierz poziom głośności",
+        type: "INTEGER",
+        required: true,
+        choices: [
+          { name: "0%", value: 0 },
+          { name: "25%", value: 25 },
+          { name: "50%", value: 50 },
+          { name: "75%", value: 75 },
+          { name: "100%", value: 100 },
+          { name: "125%", value: 125 },
+          { name: "150%", value: 150 },
+          { name: "175%", value: 175 },
+          { name: "200%", value: 200 }
+        ],
+      },
+    ],
+  },
+  async execute(interaction) {
+    const bot = interaction.client;
     const volumeLevel = await interaction.options.getInteger("poziom", true);
 
     const queue = bot.player.getQueue(interaction.guild.id);
