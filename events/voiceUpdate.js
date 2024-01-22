f33c204931371d8f565b5da1e95e6678c8c3faa4
Michal <michal@michalarch.local>
2024-01-22 22:46:52 +0100

    fix notifications

--- events/voiceUpdate.js
+++ events/voiceUpdate.js
@@ -6,10 +6,13 @@ module.exports = {
   once: false,
   async execute(oldState, newState, client) {
     if (newState.member.user.bot) return;
-    
+    console.log(
+      `${newState.member.user.tag} dołączył do kanału głosowego ${newState?.channel?.name}.`
+    );
+    // console.log(newState);
     // Sprawdzanie, czy użytkownik wszedł na konkretny kanał głosowy
-    const targetVoiceChannelID = "1198762609571278908";
-    if (newState.channelID === targetVoiceChannelID) {
+    const targetVoiceChannelId = "1198762609571278908";
+    if (newState.channelId === targetVoiceChannelId) {
       // Pobierz kanał tekstowy, na którym chcesz wysłać wiadomość
       const targetTextChannel = newState.guild.channels.cache.get("1198719827020370001");
 
@@ -19,7 +22,7 @@ module.exports = {
         const userName = newState.member.displayName;
 
         // Wyślij wiadomość na kanał tekstowy z nazwą użytkownika
-        targetTextChannel.send(`${userName} wszedł na <#1198762609571278908> <@1049729342420287508>!`);
+        targetTextChannel.send(`## ${userName} wszedł na <#1198762609571278908>! <@1049729342420287508> idź go zweryfikować!`);
       } else {
         console.error("Kanał tekstowy nie został znaleziony.");
       }
@@ -27,9 +30,7 @@ module.exports = {
     
 
     const interval = client.inactivity.get(newState.guild.id);
-    // console.log(
-    //   `${newState.member.user.tag} dołączył do kanału głosowego ${newState.channel.name}.`
-    // );
+    
     if (interval && !interval?._destroyed)
       resetUserInactivity(newState.member.user.id, newState.guild.id);
   },
