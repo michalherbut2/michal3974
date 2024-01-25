097892bc85ed8b6e3bdb40a5f6761136cd84dd4a
Michal <michal@michalarch.local>
2024-01-25 20:22:41 +0100

    change channel in notify

--- computings/notifyModerators.js
+++ computings/notifyModerators.js
@@ -2,7 +2,7 @@ const { PermissionsBitField } = require("discord.js");
 
 let isCooldown = false;
 const verificationChannelId = "1198762609571278908";
-const adminChannelId = "1047156981686874172";
+const textChannelId = "1198763617986826360";
 const moderatorRoleId = "1049729342420287508";
 const adminRoldId = "1008287145657647105";
 const cooldownTime = 60_000; // Cooldown time in milliseconds
@@ -34,14 +34,14 @@ module.exports = newState => {
   setTimeout(() => (isCooldown = false), cooldownTime);
 
   // Get the text channel where you want to send the message
-  const adminChannel =
-    newState.guild.channels.cache.get(adminChannelId);
+  const textChannel =
+    newState.guild.channels.cache.get(textChannelId);
 
   // Check if the text channel exists
-  if (adminChannel) {
+  if (textChannel) {
     // Send a message to the text channel
     const userId = newState.member.id;
-    adminChannel.send(
+    textChannel.send(
       `### <@${userId}> wszedł na <#${verificationChannelId}> <t:${parseInt(
         new Date().getTime() / 1000
       )}:R>! <@&${adminRoldId}> lub <@&${moderatorRoleId}> idź go zweryfikować!`
