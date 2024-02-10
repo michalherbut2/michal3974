924cc2f358ffad979235fbbb4b2b58eaf27e2160
PatrykPatryk5 <128894397+PatrykPatryk5@users.noreply.github.com>
2024-02-10 19:43:52 +0100

    Update getResource.js
    Super jakość i poprawki

--- computings/getResource.js
+++ computings/getResource.js
@@ -1,22 +1,31 @@
-const { createAudioResource } = require("@discordjs/voice");
-const play = require("play-dl");
+const { createAudioResource, createAudioPlayer, joinVoiceChannel, StreamType, AudioPlayerStatus } = require('@discordjs/voice');
+const { OpusEncoder } = require('@discordjs/opus');
+const play = require('play-dl');
+const ytdl = require('ytdl-core-discord');
 
 module.exports = async song => {
   const yt_info = await play.search(song, {
     limit: 1,
   });
-  
+
   const { url, title, durationRaw } = yt_info[0];
-  
-  const { stream } = await play.stream(url, {
-    discordPlayerCompatibility: true,
-  });
-  
-  const resource = createAudioResource(stream);
+
+  const stream = await ytdl(url);
+
+  // Create the encoder.
+  // Specify 48kHz sampling rate and 2 channel size.
+  const encoder = new OpusEncoder(48000, 2);
+
+  // Encode and decode.
+  const encoded = encoder.encode(stream);
+  const decoded = encoder.decode(encoded);
+
+  const resource = createAudioResource(decoded, { inputType: StreamType.Opus });
   resource.metadata = {
     title,
     duration: durationRaw,
-    stream 
+    stream: decoded
   };
-  return resource
+
+  return resource;
 }
