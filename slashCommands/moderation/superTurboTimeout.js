a5929d24ae5897ed24c98e76375308409eca61e9
Michal <michal@michalarch.local>
2024-04-10 22:28:34 +0200

    fix timeout message 2

--- slashCommands/moderation/superTurboTimeout.js
+++ slashCommands/moderation/superTurboTimeout.js
@@ -34,7 +34,7 @@ module.exports = {
 
       console.log("siema");
       await sendEmbed(interaction, {
-        description: `# ${member} na szczęsie dostał bana na 28 dni!`,
+        description: `# ${member} na szczęście dostał bana na 28 dni!`,
       });
 
       // interaction.followUp("nara");
